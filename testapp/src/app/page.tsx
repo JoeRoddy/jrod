7e3b5e01294b4abbcd6176bbeb86f2277162d437
Joe Roddy <mrjoeroddy@gmail.com>
2025-08-10 11:57:26 -0400

    Initial commit from mknext script

---
+++ testapp/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-red-50 flex items-center justify-center text-3xl text-center text-fuchsia-500">
+      hello world
+    </div>
+  );
+}
