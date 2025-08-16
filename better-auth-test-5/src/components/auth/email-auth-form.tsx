0077d05da0300fcf7822b30d77b7848b13abcb7f
Joe Roddy <mrjoeroddy@gmail.com>
2025-08-16 18:30:34 -0400

    Initial commit from mknext script

--- better-auth-test-5/src/components/auth/email-auth-form.tsx
+++ better-auth-test-5/src/components/auth/email-auth-form.tsx
@@ -152,12 +152,3 @@ export function EmailAuthForm({ mode }: { mode: EmailAuthMode }) {
     </div>
   );
 }
-
-# create the auth client
-catp src/lib/auth-client.ts <<'EOF'
-import { createAuthClient } from "better-auth/react"
-
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL
-})
